7b3e8ecd7ca1ee560f048518e9cab01bccb3c1f5
Allan1418 <allannb02@gmail.com>
2024-11-25 16:04:20 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Talent_Trade.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
