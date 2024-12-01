cb89ada6e5d218b910e15f1daf5a96ff1e382937
Allan1418 <allannb02@gmail.com>
2024-12-01 14:02:04 -0600

    connection to mongo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using MongoDB.Bson;
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -23,5 +26,31 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+// Prueba Mongo
+try
+{
+    var connectionString = builder.Configuration.GetConnectionString("MongoDB");
+    var client = new MongoClient(connectionString);
+
+    var database = client.GetDatabase("Talent_Hub");
+
+    bool isMongoLive = database.RunCommandAsync((Command<BsonDocument>)"{ping:1}").Wait(1000);
+
+    if (!isMongoLive)
+    {
+        throw new TimeoutException();
+    }
+
+    Console.WriteLine($"***Conexion exitosa a la base de datos: {database.DatabaseNamespace} ***");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"***Error al conectar a la base de datos: {ex.Message} ***");
+    Environment.Exit(1);
+}
+
+
 
 app.Run();
