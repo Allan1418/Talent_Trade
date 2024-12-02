dc0aa11f9d5e41e4cee36c9ed4bb6c2a035227f6
Allan1418 <allannb02@gmail.com>
2024-12-01 19:46:13 -0600

    creacion de clase model Usuario

---
+++ Models/Usuario.cs
@@ -0,0 +1,44 @@
+﻿
+namespace Talent_Trade.Models
+{
+
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
+
+    public class Usuario
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public required string Id { get; set; }
+
+        [BsonElement("nombreCompleto")]
+        public required string NombreCompleto { get; set; }
+
+        [BsonElement("userName")]
+        public required string UserName { get; set; }
+
+        [BsonElement("email")]
+        public required string Email { get; set; }
+
+        [BsonElement("password")]
+        public required string Password { get; set; }
+
+        [BsonElement("fechaRegistro")]
+        public DateTime FechaRegistro { get; set; }
+
+        [BsonElement("ImagePerfil")]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? ImagePerfil { get; set; }
+
+        [BsonElement("idDeCreador")]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? IdDeCreador { get; set; }
+
+        [BsonElement("suscripciones")]
+        public List<string>? Suscripciones { get; set; }
+
+        [BsonElement("facturas")]
+        public List<string>? Facturas { get; set; }
+    }
+
+}
