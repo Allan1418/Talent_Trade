2048b3d90272ca04e684ba458736612c4f2ad438
Anyelo <anyevargas30907@gmail.com>
2024-12-08 14:05:06 -0600

    Se agrego botones para Usuario#, Creador#,  y funcionalidad de carrusel en creador

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,56 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slide');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+let currentSlide = 0;
+
+// Function to show the specified slide
+function showSlide(n) {
+    slides.forEach((slide) => {
+        slide.style.display = 'none';
+    });
+    slides[n].style.display = 'block';
+}
+
+// Initial display of the first slide
+showSlide(currentSlide);
+
+// Event listener for the "next" button
+nextBtn.addEventListener('click', () => {
+    currentSlide = (currentSlide + 1) % slides.length; // Loop back to the beginning
+    showSlide(currentSlide);
+});
+
+// Event listener for the "prev" button
+prevBtn.addEventListener('click', () => {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length; // Loop back to the end
+    showSlide(currentSlide);
+});
+
+
+
+
+var modal = document.getElementById('modal');
+
+var btn = document.getElementById('modifyDataButton');
+
+var span = document.getElementById('closeModal');
+
+var form = document.getElementById('modifyDataForm');
+
+btn.onclick = function () {
+    modal.style.display = "block";
+};
+
+span.onclick = function () {
+    modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
