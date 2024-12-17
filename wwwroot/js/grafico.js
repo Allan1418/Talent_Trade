82642a94c51fa82749e8b684d793f482b795c77a
Anyelo <anyevargas30907@gmail.com>
2024-12-17 10:41:33 -0600

    Se agrego respuestas y comentarios a publicacion y form suscribirse a creador

---
+++ wwwroot/js/grafico.js
@@ -0,0 +1,21 @@
+﻿var ctx = document.getElementById('gananciasChart').getContext('2d');
+var gananciasChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: @Html.Raw(Json.Serialize(Model.MesesGanancias.Select(m => $"{m.Month}/{m.Year}"))),
+        datasets: [{
+            label: 'Ganancias Mensuales',
+            data: @Html.Raw(Json.Serialize(Model.MesesGanancias.Select(m => m.Total))),
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
